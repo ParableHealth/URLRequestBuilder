5a69fbc42d486d4976ea6987e3fe7e0762777a11
Oleg Dreyman <dreymonde@me.com>
2022-07-20 20:31:05 +0200

    update README

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # URLRequestBuilder
 
-A description of this package.
+Deal with query items, HTTP headers, request body and more in an easy, declarative way
+
+## Showcase
+
+```swift
+let urlRequest = try URLRequestBuilder(path: "users/submit")
+    .method(.post)
+    .jsonBody(user)
+    .contentType(.applicationJSON)
+    .accept(.applicationJSON)
+    .timeout(20)
+    .queryItem(name: "city", value: "San Francisco")
+    .header(name: "Auth-Token", value: authToken)
+    .makeRequest(withBaseURL: testURL)
+```
--- Tests/URLRequestBuilderTests/URLRequestBuilderTests.swift
+++ Tests/URLRequestBuilderTests/URLRequestBuilderTests.swift
@@ -11,4 +11,21 @@ final class URLRequestBuilderTests: XCTestCase {
         XCTAssertEqual(request.value(forHTTPHeaderField: "Test-Multiple"), "A,B,C")
         XCTAssertNotEqual(request.value(forHTTPHeaderField: "Test-Multiple"), "A,B")
     }
+    
+    func readme() throws {
+        let user = 1
+        let authToken = "aaa"
+        
+        let urlRequest = try URLRequestBuilder(path: "users/submit")
+            .method(.post)
+            .jsonBody(user)
+            .contentType(.applicationJSON)
+            .accept(.applicationJSON)
+            .timeout(20)
+            .queryItem(name: "city", value: "San Francisco")
+            .header(name: "Auth-Token", value: authToken)
+            .makeRequest(withBaseURL: testURL)
+        
+        print(urlRequest)
+    }
 }
